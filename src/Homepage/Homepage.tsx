5c063b8c12d00a62b1e52b7406bab566e5390f2d
yourally2 <skullspine9@gmail.com>
2021-03-16 23:20:54 +0800

    Done up basic Homepage Added a bunch of fonts

--- src/Homepage/Homepage.tsx
+++ src/Homepage/Homepage.tsx
@@ -1,12 +1,26 @@
 import React from "react";
 import NaviBar from "../Shared/NaviBar";
-
+import wordLogo from "./BriidgeWordLogo.png";
+import './Homepage.css';
 
 const Homepage = () => {
-
     return (
         <div>
             <NaviBar/>
+            <div className="row">
+                <div className="column" style={{"backgroundColor":"#0051FF"}}>
+                    <img src={wordLogo} alt="Briidge Word Logo" width="700" height="700" style={{"margin":"auto", "display":"block"}}/>
+                </div>
+                <div className="column">
+                    <h2 style={{"marginTop": 180, "marginBottom": 40}}>Who We Are</h2>
+                    <p>Briidge is a revolutionary matchmaking platform that aims to bridge students and tutors together through the use of our personalised smart algorithm.</p>
+                    <p>We also aim to ease information sharing between students by connecting them through our social networking platform.</p>
+                    <p>
+                    At briidge, we strongly believe that everyone has an equal right to high-quality education, regardless of any barrier.
+                    </p>
+                </div>
+
+            </div>
         </div>
     );
 };
--- src/Shared/NaviBar.tsx
+++ src/Shared/NaviBar.tsx
@@ -1,28 +1,29 @@
 import React from "react";
-import {Navbar, Nav, NavDropdown, Form, FormControl, Button} from 'react-bootstrap';
+import {Navbar, Nav, Form, FormControl, Button} from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import './NaviBar.css';
-import logo from "./BriidgeIcon.png";
+import logo from "./BriidgeIconTransparent.png";
 
 const NaviBar = () => {
 
     return (
-        <Navbar className="color-nav" expand="lg">
-        {/* <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand> */}
-        <img src={logo} alt="Briidge Logo" width="50" height="50" />
+        <Navbar className="color-nav" expand="md">
+        <Link to="/">
+            <img src={logo} alt="Briidge Logo" width="50" height="35" style={{"marginRight":15}}/>
+        </Link>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="font-nav mr-auto">
-                <Nav.Link className="font-nav" href="#home">Home</Nav.Link>
+            <Nav className="mr-auto">
                 <Nav.Link className="font-nav" href="#link">Our Plans</Nav.Link>
                 <Nav.Link className="font-nav" href="#link">About Us</Nav.Link>
                 <Nav.Link className="font-nav" href="#link">Our Tutors</Nav.Link>
                 <Nav.Link className="font-nav" href="#link">Social Network</Nav.Link>
                 <Nav.Link className="font-nav" href="#link">FAQs</Nav.Link>
             </Nav>
-            <Form inline>
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            <Button variant="outline-success">Search</Button>
+            <Form>
+                <button type="button" className="btn btn-primary" style={{"marginRight":10}}>Login</button>
+                <button type="button" className="btn btn-primary">Sign up</button>
             </Form>
         </Navbar.Collapse>
         </Navbar>
