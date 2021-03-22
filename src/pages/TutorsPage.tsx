3f3216ce6b0cffa7651914fb22b7415e535f1d9a
ganhongyao <ganhongyao@u.nus.edu>
2021-03-22 10:57:34 +0800

    added button/link to tutors signup page

--- src/pages/TutorsPage.tsx
+++ src/pages/TutorsPage.tsx
@@ -2,10 +2,28 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import NaviBar from '../components/NaviBar';
 import AuthHeader from '../components/AuthHeader';
+import { Button } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
     root: {
         textAlign: 'center',
+    },
+
+    container: {
+        backgroundColor: '#0051ff',
+        height: '150px',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+
+    cta: {
+        color: '#0051ff',
+        fontSize: '125%',
+        textTransform: 'none',
+        borderRadius: '25px/75px',
     }
 }));
 
@@ -16,6 +34,13 @@ export default function TutorsPage() {
         <div className={classes.root}>
             <NaviBar />     
             <AuthHeader text={"Our Tutor Network"} />
+            <div className={classes.container}>
+                <Button className={classes.cta} component={Link} to="/signup" 
+                    variant="contained" color="secondary" size="large">
+                    Join the Briidge Tutor Community Now!
+                </Button>
+            </div>
+            
         </div>
     )
 }
