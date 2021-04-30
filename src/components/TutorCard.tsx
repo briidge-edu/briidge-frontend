7d2919b57b9eff954f7e2573b2eb49d84eb242c8
ganhongyao <ganhongyao@u.nus.edu>
2021-04-30 15:19:34 +0800

    made tutors page more responsive to different screen sizes

--- src/components/TutorCard.tsx
+++ src/components/TutorCard.tsx
@@ -1,16 +1,11 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Typography } from '@material-ui/core';
+import { Grid, Typography } from '@material-ui/core';
 import image from '../Shared/tutor.jpeg'
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        flexWrap: 'wrap',
-        marginLeft: '10px',
-        marginRight: '10px'
+        marginBottom: '3%'
     },
 
     image: {
@@ -24,7 +19,7 @@ export default function TutorCard() {
     const classes = useStyles();
 
     return (
-        <div className={classes.root}>
+        <Grid className={classes.root} item xs={6} lg={3} xl={2}>
             <img src={image} className={classes.image} alt=''/>
             <Typography>Alberto Connelli</Typography>
             <Typography variant="subtitle2" style={{fontFamily: 'Montserrat-Light'}}>
@@ -36,9 +31,9 @@ export default function TutorCard() {
             <Typography variant="subtitle2" style={{fontFamily: 'Montserrat-Light'}}>
                 Student at National University of Singapore
             </Typography>
-            <Typography variant="subtitle2" style={{fontFamily: 'Montserrat-Light'}}>
+            <Typography variant="subtitle2" style={{fontFamily: 'Montserrat-Light', margin: '0 auto'}}>
                 I have been teaching Chemistry and Physics to Upper Secondary students.
             </Typography>
-        </div>
+        </Grid>
     )
 }
