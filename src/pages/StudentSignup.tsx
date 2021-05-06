237ffd3fe88da8d8f500fbdf0641cba92968d94a
ganhongyao <ganhongyao@u.nus.edu>
2021-05-06 21:39:40 +0800

    added stepper for student signup form

--- src/pages/StudentSignup.tsx
+++ src/pages/StudentSignup.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AuthHeader from '../components/AuthHeader'
-import { Button, FormControl, InputLabel, MenuItem, Select, Typography } from '@material-ui/core';
+import { Button, FormControl, InputLabel, MenuItem, Select, Step, StepLabel, Stepper, Typography } from '@material-ui/core';
 import NaviBar from '../components/NaviBar';
 
 const useStyles = makeStyles((theme) => ({
@@ -12,7 +12,7 @@ const useStyles = makeStyles((theme) => ({
 
     formControl: {
         marginTop: '2%',
-        minWidth: 240
+        minWidth: 240,
     },
 
     upload: {
@@ -21,7 +21,6 @@ const useStyles = makeStyles((theme) => ({
     },
 
     input: {
-        display: 'none'
     },
 
     button: {
@@ -30,12 +29,18 @@ const useStyles = makeStyles((theme) => ({
 
 }));
 
+function getSteps() {
+    return ['Select master blaster campaign settings', 'Create an ad group', 'Create an ad'];
+  }
+
 export default function StudentSignup() {
     const classes = useStyles();
 
-    const [educationLevel, setEducationLevel] = useState("")
+    const [activeStep, setActiveStep] = useState(0);
+    const steps = getSteps();
+    const [educationLevel, setEducationLevel] = useState("");
 
-    const handleChange = (event:any) => {
+    const handleChange = (event: any) => {
         setEducationLevel(event.target.value);
     };
 
@@ -43,38 +48,55 @@ export default function StudentSignup() {
         <div className={classes.root}>
             <NaviBar />
             <AuthHeader text="Student Sign-Up" />
-            <FormControl variant="outlined" className={classes.formControl}>
-            <InputLabel>Level of Education</InputLabel>
-                <Select
-                    labelId="demo-simple-select-outlined-label"
-                    id="demo-simple-select-outlined"
-                    value={educationLevel}
-                    onChange={handleChange}
-                    label="Level of Education"
+            <Stepper activeStep={activeStep} alternativeLabel>
+                {steps.map((label) => (
+                <Step key={label}>
+                    <StepLabel>{label}</StepLabel>
+                </Step>
+                ))}
+            </Stepper>
+            <div>
+                <FormControl variant="outlined" className={classes.formControl}>
+                    <InputLabel>Level of Education</InputLabel>
+                    <Select
+                        labelId="demo-simple-select-outlined-label"
+                        id="demo-simple-select-outlined"
+                        value={educationLevel}
+                        onChange={handleChange}
+                        label="Level of Education"
                     >
-                    <MenuItem value={"primary"}>Primary</MenuItem>
-                    <MenuItem value={"secondary"}>Secondary</MenuItem>
-                    <MenuItem value={"jc"}>Junior College</MenuItem>
-                </Select>
-            </FormControl>
-            <div className={classes.upload}>
-                <Typography variant="h5">Join under "BriidgeAssist"</Typography>
-                <Typography variant="subtitle1">Documentary proof is required for verification purposes.</Typography>     
-                <input
-                    accept="image/*"
-                    className={classes.input}
-                    id="contained-button-file"
-                    multiple
-                    type="file"
-                />
-                <label htmlFor="contained-button-file">
-                    <Button variant="contained" color="primary" component="span">
-                    Upload
-                    </Button>
-                </label>    
+                        <MenuItem value={"primary"}>Primary</MenuItem>
+                        <MenuItem value={"secondary"}>Secondary</MenuItem>
+                        <MenuItem value={"jc"}>Junior College</MenuItem>
+                    </Select>
+                </FormControl>
+                <div className={classes.upload}>
+                    <Typography variant="h5">Join under "BriidgeAssist"</Typography>
+                    <Typography variant="subtitle1">Documentary proof is required for verification purposes.</Typography>
+                    <input
+                        accept="image/*"
+                        className={classes.input}
+                        id="contained-button-file"
+                        multiple
+                        type="file"
+                    />
+                    <label htmlFor="contained-button-file">
+                        <Button variant="contained" color="primary" component="span">
+                            Upload
+                        </Button>
+                    </label>
+                </div>
             </div>
-                  
-            <Button variant="contained" color="primary" className={classes.button} type="submit">
+
+            <Button 
+                variant="contained" 
+                color="primary" 
+                className={classes.button} 
+                >
+            Back
+            </Button>
+
+            <Button variant="contained" color="primary" className={classes.button} >
                 Next
             </Button>
         </div>
